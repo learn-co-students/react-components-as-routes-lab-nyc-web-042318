8df3559ccf6a178de0c50ece9fcb0c8f5296321a
cupofjoy <eelantung@gmail.com>
2018-07-01 14:46:32 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorList = () => {
+    return directors.map(function(director) {
+      return (
+        <div>
+          <h1>{director.name}</h1>
+          <ul>
+            {director.movies.map( movie => {
+              return <li>{movie}</li>
+            })}
+          </ul>
+        </div>
+      )})
+  }
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorList()}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,29 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+  const movieList = () => {
+    return movies.map(function(movie) {
+      return(
+        <div>
+          <h1>{movie.title}</h1>
+          <h3>{movie.time}</h3>
+          <h3>Genres:</h3>
+          <ul>
+            {movie.genres.map(function(genre) {
+              return(
+                <li>{genre}</li>
+              )
+            })}
+          </ul>
+        </div>
+      )
+    })
+  }
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movieList()}
     </div>
   );
 };
