5a694d68fec4a63fcd2970406f22f8aa39b9968b
ensallee <ensallee@gmail.com>
2018-07-01 11:52:41 -0400

    done

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,23 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  console.log(actors)
+
+  let actorDivs = actors.map((actor) => {
+    return <div>
+      <h1>{actor.name}</h1>
+      <ul>
+        {actor.movies.map((movie) => {
+          return <li>{movie}</li>
+        })}
+      </ul>
+    </div>
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorDivs}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+
+  let directorDivs = directors.map((dir) => {
+    return <div>
+      <h1>{dir.name}</h1>
+      <ul>
+        {dir.movies.map((movie) => {
+          return <li>{movie}</li>
+        })}
+      </ul>
+    </div>
+  })
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorDivs}
     </div>
   );
 }
