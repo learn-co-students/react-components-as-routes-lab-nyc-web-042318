7f3c35722e234247a43288fc6ca7066496f3e54a
Canino Developments <ian.canino@gmail.com>
2018-07-26 01:04:19 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { movies } from '../data';
+import React from 'react'
+import { movies } from '../data'
 
 const Movies = () => {
+  let divMovies = movies.map((movie) =>
+    <div>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <p>Genre: {movie.genres.map((genre) => <i>{genre}</i>)}</p>
+    </div>
+  )
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {divMovies}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink
+        to='/'
+        exact
+      >Home</NavLink>
+      <NavLink
+        to='/movies'
+        exact
+      >Movies</NavLink>
+      <NavLink
+        to='/directors'
+        exact
+      >Directors</NavLink>
+      <NavLink
+        to='/actors'
+        exact
+      >Actors</NavLink>
     </div>
-  );
-};
+  )
+}
 
-export default NavBar;
+export default NavBar
